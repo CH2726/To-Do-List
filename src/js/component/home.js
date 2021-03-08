ad2bcbc07cf456d3dc21859f6f4a1df546e8ac3c
chubbard91 <chubbard91@gmail.com>
2021-03-08 23:23:58 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,9 +5,22 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [todoList, setTodoList] = useState([]);
+	const getTodos = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/joaohxavier")
+			.then(response => response.json())
+			.then(res => setTodoList(res));
+	};
+	useEffect(() => {
+		getTodos();
+	}, []);
+	console.log(todoList);
 	return (
 		<div className="text-center mt-5">
 			<h1>To-Do List</h1>
+			{todoList.map((item, index) => (
+				<p key={index}>{item.label}</p>
+			))}
 		</div>
 	);
 }
